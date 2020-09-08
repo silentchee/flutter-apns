4aeb7105bf3da54bfbddccb7c8937d81024a5441
Tang Woon Chee <woonchee.tang@ifastfinancial.com>
2020-09-08 10:37:24 +0800

    adding huawei push notification support

--- lib/apns.dart
+++ lib/apns.dart
@@ -1,13 +1,25 @@
 import 'dart:io';
 
+import 'package:flutter/services.dart';
 import 'package:flutter_apns/apns_connector.dart';
 import 'package:flutter_apns/connector.dart';
 import 'package:flutter_apns/firebase_connector.dart';
+import 'package:flutter_apns/huawei_connector.dart';
+import 'package:google_api_availability/google_api_availability.dart';
 
 /// Creates either APNS or Firebase connector to manage the push notification registration.
-PushConnector createPushConnector() {
+Future<PushConnector> createPushConnector() async {
   if (Platform.isAndroid) {
-    return FirebasePushConnector();
+     GooglePlayServicesAvailability availability;
+    try {
+      availability = await GoogleApiAvailability.instance.checkGooglePlayServicesAvailability();
+    }on PlatformException {
+      availability = GooglePlayServicesAvailability.unknown;
+    }
+    if(availability == GooglePlayServicesAvailability.success){
+      return FirebasePushConnector();
+    }
+    return HuaweiPushConnector();
   } else {
     return ApnsPushConnector();
   }
