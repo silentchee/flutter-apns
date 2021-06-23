0af95c7ddd50aaf5c26dc10f98f539c384265e32
Tang Woon Chee <woonchee.tang@ifastfinancial.com>
2021-06-23 22:27:06 +0800

    Remove huawei

--- lib/apns.dart
+++ lib/apns.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/services.dart';
-import 'package:flutter_apns/src/huawei_connector.dart';
+// import 'package:flutter_apns/src/huawei_connector.dart';
 import 'package:google_api_availability/google_api_availability.dart';
 import 'package:flutter_apns/src/apns_connector.dart';
 import 'package:flutter_apns/src/connector.dart';
@@ -21,7 +21,7 @@ Future<PushConnector> createPushConnector() async {
     if(availability == GooglePlayServicesAvailability.success){
       return FirebasePushConnector();
     }
-    return HuaweiPushConnector();
+    return null;
   } else {
     return ApnsPushConnector();
   }
